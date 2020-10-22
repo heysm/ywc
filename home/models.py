e25484502ae3311a2fd1412db48ad43a62e39964
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-10-22 22:15:05 +0530

    return name of image

--- home/models.py
+++ home/models.py
@@ -7,3 +7,6 @@ class Gallery(models.Model):
     name = models.CharField(max_length=200, default="YWC")
     caption = models.TextField(max_length=500, default="you were chosen")
     image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
