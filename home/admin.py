22ffe4c32b1c01c103de98bbb9d271ebd1a1a689
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-10-23 00:09:44 +0530

    posts

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gallery
+from .models import Gallery, Posts
 
 admin.site.site_header = "You Were Chosen"
 admin.site.site_title = "Admin Panel"
@@ -8,3 +8,4 @@ admin.site.index_title = "Welcome to the YWC admin area"
 
 # Register your models here.
 admin.site.register(Gallery)
+admin.site.register(Posts)
--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 
 
@@ -7,6 +7,24 @@ class Gallery(models.Model):
     name = models.CharField(max_length=200, default="YWC")
     caption = models.TextField(max_length=500, default="you were chosen")
     image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Posts(models.Model):
+    name = models.CharField(max_length=200, default='ywc art')
+    artist = models.CharField(max_length=400, default="annonymous")
+    category = models.CharField(max_length=200, blank=True)
+    video_url = models.URLField(blank=True)
+    image_url = models.URLField(blank=True)
+    date = models.DateField(default=datetime.now)
+    caption = models.TextField(default='''You Were Chosen is a new line of thought.
+
+                               A line of thought to make people believe in their dreams, make people take authority of themselves and make a diverse and creative future.
+
+                               Most importantly, to build a society that can learn to love each other, a society to feel that their lives have meaning, a society to feel that they are important.''')
+    popularity = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
