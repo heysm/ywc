4d33db28b41ca5aef59c6050d06c7c698249c3a1
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-10-23 14:29:35 +0530

    logout

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ app_name = 'user'
 urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('logoutuser/', views.logoutuser, name="logoutuser"),
 ]
--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 # Create your views here.
 
 
@@ -26,3 +26,9 @@ def signupuser(request):
 
 def dashboard(request):
     return render(request, 'user/dashboard.html')
+
+
+def logoutuser(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('home:home')
