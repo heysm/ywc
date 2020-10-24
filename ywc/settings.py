3a8bdcdb94e4dbd58a113004ec6d20740af5dc6f
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-10-24 20:04:14 +0530

    heroku postgress

--- ywc/settings.py
+++ ywc/settings.py
@@ -89,8 +89,13 @@ DATABASES = {
 }
 
 
-DATABASES['default'] = dj_database_url.config(
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600, ssl_require=True)
+
+db_from_env = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
